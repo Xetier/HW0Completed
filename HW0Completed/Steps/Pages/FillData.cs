04c3b9670227df806a131521ac6230ef18132f1b
Pieter Gleixner <pieter.gleixner@cignium.com>
2019-01-28 11:01:47 -0500

    refactor with library

--- HW0Completed/Steps/Pages/FillData.cs
+++ HW0Completed/Steps/Pages/FillData.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using Cignium.Contrib.GaugeSelenium;
+using Cignium.Contrib.GaugeSelenium.Impl;
+using FluentAssertions;
 using Gauge.CSharp.Lib.Attribute;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
@@ -29,16 +31,14 @@ namespace HW0Completed.Steps.Pages
         [Step("Fill first name in customer_firstname field")]
         public void FillFirstName()
         {
-            _driver.FindElement(By.Id("customer_firstname")).Clear();
-            _driver.FindElement(By.Id("customer_firstname")).SendKeys(firstName);
+            SendDataString("Id", "customer_firstname", firstName);
             Log.Information("firstName: " + firstName);
         }
 
         [Step("Fill last name in customer_lastname field")]
         public void FillLastName()
         {
-            _driver.FindElement(By.Id("customer_lastname")).Clear();
-            _driver.FindElement(By.Id("customer_lastname")).SendKeys(lastName);
+            SendDataString("Id", "customer_lastname", lastName);
             Log.Information("lastName: " + lastName);
         }
 
@@ -46,8 +46,7 @@ namespace HW0Completed.Steps.Pages
         public void FillPassword()
         {
             string password = RandomGeneratePass(10).ToString();
-            _driver.FindElement(By.Id("passwd")).Clear();
-            _driver.FindElement(By.Id("passwd")).SendKeys(password);
+            SendDataString("Id", "passwd", password);
             Log.Information("password: " + password);
         }
 
@@ -55,122 +54,115 @@ namespace HW0Completed.Steps.Pages
         public void PickBirthdayDay()
         {
             int day = random.Next(1, 31);
-            var cbxBirthDays = _driver.FindElement(By.Id("days"));
-            var selectElementBirthDays = new SelectElement(cbxBirthDays);
-            selectElementBirthDays.SelectByValue(day.ToString());
+            SendDataCmbx("Id", "days", day.ToString());
+            Log.Information("BirthdayDay: " + day.ToString());
         }
 
         [Step("Pick month in birthday month combobox")]
         public void PickBirthdayMonth()
         {
             int month = random.Next(1, 13);
-            var cbxBirthMonth = _driver.FindElement(By.Id("months"));
-            var selectElementBirthMonth = new SelectElement(cbxBirthMonth);
-            selectElementBirthMonth.SelectByValue(month.ToString());
+            SendDataCmbx("Id", "months", month.ToString());
+            Log.Information("BirthdayMonth: " + month.ToString());
         }
 
         [Step("Pick year in birthday year combobox")]
         public void PickBirthdayYear()
         {
             int year = random.Next(1900, 2020);
-            var cbxBirthYear = _driver.FindElement(By.Id("years"));
-            var selectElementBirthYear = new SelectElement(cbxBirthYear);
-            selectElementBirthYear.SelectByValue(year.ToString());
+            SendDataCmbx("Id", "years", year.ToString());
+            Log.Information("BirthdayYear: " + year.ToString());
         }
 
         [Step("Check Sign up for our newsletter! and Receive special offers from our partners!")]
         public void CheckNewsletterandSpecialOffers()
         {
-            _driver.FindElement(By.Id("newsletter")).Click();
-            _driver.FindElement(By.Id("optin")).Click();
+            _driver.FindElementBy("Id", "newsletter").Click();
+            _driver.FindElementBy("Id", "optin").Click();
         }
 
         [Step("Fill Company name in company field")]
         public void FillCompanyName()
         {
             string company = RandomGenerate(10).ToString();
-            _driver.FindElement(By.Id("company")).Clear();
-            _driver.FindElement(By.Id("company")).SendKeys(company);
+            SendDataString("Id", "company", company);
+            Log.Information("CompanyName: " + company);
         }
 
         [Step("Fill first address in Address1 field")]
         public void FillFirstAddress()
         {
             string address1 = RandomGenerate(10).ToString();
-            _driver.FindElement(By.Id("address1")).Clear();
-            _driver.FindElement(By.Id("address1")).SendKeys(address1);
+            SendDataString("Id", "address1", address1);
+            Log.Information("address1: " + address1);
         }
 
         [Step("Fill second address in Address2 field")]
         public void FillSecondAddress()
         {
             string address2 = RandomGenerate(10).ToString();
-            _driver.FindElement(By.Id("address2")).Clear();
-            _driver.FindElement(By.Id("address2")).SendKeys(address2);
+            SendDataString("Id", "address2", address2);
+            Log.Information("address2: " + address2);
         }
 
         [Step("Fill city in City field")]
         public void FillCity()
         {
             string city = RandomGenerate(10).ToString();
-            _driver.FindElement(By.Id("city")).Clear();
-            _driver.FindElement(By.Id("city")).SendKeys(city);
+            SendDataString("Id", "city", city);
+            Log.Information("city: " + city);
         }
 
         [Step("Pick state in state combobox")]
         public void PickState()
         {
             int state = random.Next(1, 51);
-            var cbxState = _driver.FindElement(By.Id("id_state"));
-            var selectElementState = new SelectElement(cbxState);
-            selectElementState.SelectByValue(state.ToString());
+            SendDataCmbx("Id", "id_state", state.ToString());
+            Log.Information("State: " + state.ToString());
         }
 
         [Step("Fill post code in postcode field")]
         public void FillPostCode()
         {
-            _driver.FindElement(By.Id("postcode")).Clear();
-            _driver.FindElement(By.Id("postcode")).SendKeys(RandomGenerateNumber(5));
+            string randPostCode = RandomGenerateNumber(5);
+            SendDataString("Id", "postcode", randPostCode);
+            Log.Information("PostCode: " + randPostCode);
         }
 
         [Step("Pick country in country combobox")]
         public void PickCountry()
         {
-            var cbxCountry = _driver.FindElement(By.Id("id_country"));
-            var selectElementCountry = new SelectElement(cbxCountry);
-            selectElementCountry.SelectByText("United States");
+            SendDataCmbx("Id", "id_country", "United States");
+            Log.Information("Country: " + "United States");
         }
 
         [Step("Fill additional information in other field")]
         public void FillAdditionalInformation()
         {
             string additional = RandomGenerate(10).ToString();
-            _driver.FindElement(By.Id("other")).Clear();
-            _driver.FindElement(By.Id("other")).SendKeys(additional);
+            SendDataString("Id", "other", additional);
+            Log.Information("Additional: " + additional);
         }
 
         [Step("Fill phone in phone field")]
         public void FillPhone()
         {
             string phone = "+" + RandomGenerateNumber(2).ToString() + " " + RandomGenerateNumber(9).ToString();
-            _driver.FindElement(By.Id("phone")).Clear();
-            _driver.FindElement(By.Id("phone")).SendKeys(phone);
+            SendDataString("Id", "phone", phone);
         }
 
         [Step("Fill mobile phone in mobile phone field")]
         public void FillMobilePhone()
         {
             string mphone = "+" + RandomGenerateNumber(2).ToString() + " " + RandomGenerateNumber(9).ToString();
-            _driver.FindElement(By.Id("phone_mobile")).Clear();
-            _driver.FindElement(By.Id("phone_mobile")).SendKeys(mphone);
+            SendDataString("Id", "phone_mobile", mphone);
         }
 
         [Step("Fill Address in alias field")]
         public void FillAddress()
         {
             string address = RandomGenerate(10).ToString();
-            _driver.FindElement(By.Id("alias")).Clear();
-            _driver.FindElement(By.Id("alias")).SendKeys(address);
+            SendDataString("Id", "alias", address);
         }
 
         [Step("Check Proper username is shown in the header")]
@@ -196,6 +188,23 @@ namespace HW0Completed.Steps.Pages
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrsqtuvwxyz!#$%&/()=?¡¿' ";
             return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static void SendDataString(string type, string selectorValue, string data)
+        {
+            IWebDriver _driver = Hooks.WebDriver;
+
+            var element = _driver.FindElementBy(type, selectorValue);
+            element.Clear();
+            element.SendKeys(data);
+        }
+
+        public static void SendDataCmbx(string type, string selectorValue, string data)
+        {
+            IWebDriver _driver = Hooks.WebDriver;
+            var cbx = _driver.FindElementBy(type, selectorValue);
+            var selectElementState = new SelectElement(cbx);
+            selectElementState.SelectByValue(data);
         }
     }
 }
